9b72b905a6cc4a6804f1f731391f20beafc34900
Micha Vie <micha@freeiam.com>
2021-03-12 16:10:26 +0100

    Add welcome responses

--- app/DiscordCommands/WelcomeDiscordCommand.php
+++ app/DiscordCommands/WelcomeDiscordCommand.php
@@ -9,6 +9,13 @@ class WelcomeDiscordCommand
     private $responses = [
         'Welcome - great to have you here!',
         'Hey Hey Hey!',
+        'Hey you - where have you been all this time? 😨',
+        'I am Ava, and who are you?',
+        '👋 Hallo! 🇩🇪',
+        '👋 Bonjour! 🇫🇷',
+        '👋 Hola! 🇪🇸',
+        '👋 Konnichiwa! 🇯🇵',
+        '👋 Nǐn hǎo! 🇨🇳',
     ];
 
     public function execute(Message $discordMessage): void
