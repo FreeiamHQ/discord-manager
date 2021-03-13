d945fc8645922074bf5ae52aba71c9314198d34a
Micha Vie <micha@freeiam.com>
2021-03-13 21:46:22 +0100

    Add app colors

---
+++ app/AppColor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class AppColor
+{
+    const Red = '#ED3258';
+    const Orange = '#F05F4D';
+    const Blue = '#273C75';
+    const Yellow = '#FBC531';
+    const White = '#F7F8FA';
+    const Gray = '#7F8FA6';
+    const GrayDark = '#353B48';
+
+    const ThemeGreen = '#48BB78';
+    const ThemeRed = '#E53E3E';
+}
