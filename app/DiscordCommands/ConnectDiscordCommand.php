c089c6ccf9f2541e6fbf7539488d59aa029184bc
micha vie <micha@freeiam.com>
2022-01-20 01:01:59 +0100

    update connect command details

--- app/DiscordCommands/ConnectDiscordCommand.php
+++ app/DiscordCommands/ConnectDiscordCommand.php
@@ -8,14 +8,14 @@ class ConnectDiscordCommand
 {
     public function execute(Message $discordMessage): void
     {
-        $helpArticleLinkUrl = 'https://support.freeiam.com/discord/sync-account.html';
+        $helpArticleLinkUrl = 'https://law.superciety.com/superhuman/discord.html';
 
         $discordMessage->channel
             ->sendMessage(
                 "Connecting your Freeiam account with Discord is **EASY**!\n\n" .
                 "**Your benefits**:\n".
-                "- Become a verified rebel\n".
-                "- Sync your Freeiam rank\n".
+                "- Get the 'Superhuman' Discord role\n".
+                "- Sync your Freeiam rank (Discord role)\n".
                 "- Auto-publish your campfires\n".
                 "- Auto-publish achieved 💯's\n\n".
                 "Just follow these __quick 4 steps__ and you are done in no time: {$helpArticleLinkUrl}"
